f56a5a84117344a6a6df682e0c1812a0008ac709
Anna <102611142+AnnatheKir@users.noreply.github.com>
2022-07-21 16:40:52 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 /*
-Современный учебник по JavaScript - https://learn.javascript.ru/
-W3School JavaScript Tutorial - https://www.w3schools.com/js/default.asp
-MDN Web Docs JavaScript - https://developer.mozilla.org/ru/docs/Learn/JavaScript/First_steps
-YouTube также будет полезен, довольно много видео по основам.
+
 */
 
 
@@ -26,7 +23,7 @@ let text = `${userName} is ${age} years old`
 console.log(text)
 
 let user = {
-    name:"igor",
+    name:"artem",
     surname:"ivanoff",
     age:25
 };
